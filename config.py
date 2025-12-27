f92c4bc9e2ce949b23617ed9981ba5ce5c7e2f64
ak4422920 <ak4422920@gmail.com>
2025-12-27 11:07:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8371607189"))
 PORT = os.environ.get("PORT", "8080")
 DB_URL = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "filesharexbot")
